393a87f7d441286cd30a48fca9a4eed59f533a2b
Daniel Chu <danchu123@gmail.com>
2016-11-23 13:09:30 -0600

    MR maze test

---
+++ Assets/Scripts/Boundary.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Boundary : MonoBehaviour {
+
+	//Game controller object set in UnityEngine.
+//	public GameObject gameControllerObject;
+//	GameController gameController;
+
+	void Start(){
+//		gameController = gameControllerObject.GetComponent<GameController> ();
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Ball") {
+			Destroy (other.gameObject);
+		}
+
+//		//Changes scene to "Restart" scene.
+//		if (gameController.gameLives < 1) {
+//			SceneManager.LoadScene ("ScoreSubmission");
+//		}
+	}
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+//		movementTest.transform.position =  gyroTest.userAcceleration;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.acceleration.x < -0.1f) {
+			moveLeft ();
+		} else if (Input.acceleration.x > 0.1f) {
+			moveRight ();
+		} 
+
+		if (Input.acceleration.y > 0.1f) {
+			moveForward ();
+		} else if (Input.acceleration.y < -0.1f) {
+			moveBackward ();
+		} 
+
+		if(Input.acceleration.x > -0.1f
+			&& Input.acceleration.x < 0.1f
+			&& Input.acceleration.y > -0.1f
+			&& Input.acceleration.y < 0.1f){
+			stopMovement ();
+		}
+	}
+
+	void moveLeft(){
+		rb.AddForce (new Vector2 (-speed, 0));
+	}
+
+	void moveRight(){
+		rb.AddForce (new Vector2 (speed, 0));
+	}
+
+	void moveForward(){
+		rb.AddForce (new Vector2 (0, speed));
+	}
+
+	void moveBackward(){
+		rb.AddForce (new Vector2 (0, -speed));
+	}
+
+	void stopMovement(){
+		rb.velocity = Vector2.zero;
+	}
+}
